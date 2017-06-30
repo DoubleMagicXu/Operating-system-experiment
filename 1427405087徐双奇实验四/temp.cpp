12a8e8e11fac5300652f38612626c2d5d32e2cf6
DoubleMagicXu <2528167446@qq.com>
2017-06-30 16:27:39 +0800

    end

--- 1427405087徐双奇实验四/temp.cpp
+++
@@ -1,9 +0,0 @@
-#include<fstream>
-#include<iostream>
-using namespace std;
-int main()
-{
-    ofstream f("/home/sqxu/Desktop/os/1427405087徐双奇实验四/temp.txt");
-    //f<<"ff"<<endl;
-    return 0;
-}
